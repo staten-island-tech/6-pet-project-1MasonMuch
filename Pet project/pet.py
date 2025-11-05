cd6aec936774dbb88f5971625a5cffeffdebf0ac
1MasonMuch <masonm126@nycstudents.net>
2025-11-05 06:27:00 -0800

    5

--- Pet project/pet.py
+++ Pet project/pet.py
@@ -1,1 +1,12 @@
-import json 
+class pet:
+    name = input("Input Your pet's name")
+    age = 1
+    happiness = 40
+
+    def _init_(self, name, age, happiness):
+        self.name = name
+        self.age = age 
+        self.happiness = happiness
+
+    def play(self):
+        
