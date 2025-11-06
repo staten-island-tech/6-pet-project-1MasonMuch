80ffdb66aed1daa481dfff0372dd2bd77d8520fe
1MasonMuch <masonm126@nycstudents.net>
2025-11-06 06:25:41 -0800

    6

--- Pet project/pet.py
+++ Pet project/pet.py
@@ -1,12 +1,22 @@
-class pet:
-    name = input("Input Your pet's name")
-    age = 1
-    happiness = 40
 
-    def _init_(self, name, age, happiness):
+class pet():
+    def __init__(self, name, happiness, age):
         self.name = name
-        self.age = age 
         self.happiness = happiness
-
+        self. age = age
+    def get_older(self, name, age):
+        age = age + 1
+        print(f"{name} is {age} years old!")
+    
     def play(self):
-        #mason lkes gooning yessirr
+        if self.happiness == 100:
+            print("Your pet is too excited")
+        elif self.happiness > 100:
+            print("Your pet is too excited")
+        else:
+            self.happiness += 10
+            print(f" You played with {self.name} and its happiness is now a {self.happiness}")
+
+name = input("Input a name for yor pet")
+age = 1
+happiness = 50
